d7a5a96244b52eb567d0ec0dfa453da82de40e14
SadeghMohammadian <sadegh803211@gmail.com>
2024-01-30 14:44:15 +0330

    AdminMenu created

--- src/main/java/view/AdminMenu.java
+++ src/main/java/view/AdminMenu.java
@@ -1,11 +1,20 @@
 package view;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import java.net.URL;
 
 public class AdminMenu extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        URL url=AdminLoginMenu.class.getResource("/FXML/AdminMenu.fxml");
+        AnchorPane anchorPane= FXMLLoader.load(url);
+        Scene scene = new Scene(anchorPane);
+        stage.setScene(scene);
 
     }
 }
