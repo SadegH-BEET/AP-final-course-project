e93958b343d8c514907c4d00ef3d4d173b7323ef
SadeghMohammadian <sadegh803211@gmail.com>
2024-01-30 16:43:53 +0330

    Admin add new restaurant logic added

--- src/main/java/view/AdminMenuController.java
+++ src/main/java/view/AdminMenuController.java
@@ -1,9 +1,16 @@
 package view;
 
+import javafx.scene.control.Alert;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 import model.Restaurant;
 
 public class AdminMenuController {
+    public Text nameTextField;
+    public Text passwordPasswordField;
+    public Text addressTextField;
+    public Text managerTextField;
+
     public void showListOfAllRestaurants(MouseEvent mouseEvent) {
         System.out.println("==============================");
         System.out.println("List of All restaurants:");
@@ -13,5 +20,25 @@ public class AdminMenuController {
             System.out.println();
         }
         System.out.println("==============================");
+    }
+
+    public void logOut(MouseEvent mouseEvent) throws Exception {
+        new AdminLoginMenu().start(Main.stage);
+    }
+
+    public void addNewRestaurant(MouseEvent mouseEvent) {
+        String name=nameTextField.getText();
+        String password=passwordPasswordField.getText();
+        String address=addressTextField.getText();
+        String managerName=managerTextField.getText();
+        if(name!="" && password!="" && address!="" && managerName!=""){
+            Restaurant restaurant=new Restaurant(name,managerName,address,password);
+
+        }
+        else{
+            Alert alert=new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("please fill the blanks");
+            alert.showAndWait();
+        }
     }
 }
