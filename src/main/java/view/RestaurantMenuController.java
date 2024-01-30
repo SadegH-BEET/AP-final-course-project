0c8f2773cffad66ed84c06284475b674abcbbae0
SadeghMohammadian <sadegh803211@gmail.com>
2024-01-30 17:33:15 +0330

    restaurant menu created and log out button logic added

--- src/main/java/view/RestaurantMenuController.java
+++ src/main/java/view/RestaurantMenuController.java
@@ -1,4 +1,12 @@
 package view;
 
+import javafx.scene.input.MouseEvent;
+import model.Restaurant;
+
 public class RestaurantMenuController {
+    public void logOut(MouseEvent mouseEvent) throws Exception {
+        Restaurant.currentRestaurant=null;
+        new Main().start(Main.stage);
+    }
+
 }
