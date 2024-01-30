fc9f367978487752e8cddc349a89965d2f72453c
SadeghMohammadian <sadegh803211@gmail.com>
2024-01-30 16:30:34 +0330

    Admin show list of All Students button and logicc controller added

--- src/main/java/view/AdminMenuController.java
+++ src/main/java/view/AdminMenuController.java
@@ -1,4 +1,17 @@
 package view;
 
+import javafx.scene.input.MouseEvent;
+import model.Restaurant;
+
 public class AdminMenuController {
+    public void showListOfAllRestaurants(MouseEvent mouseEvent) {
+        System.out.println("==============================");
+        System.out.println("List of All restaurants:");
+        for (int i=0;i< Restaurant.allRestaurants.size();i++){
+            System.out.print(i+1);
+            System.out.print(" "+Restaurant.allRestaurants.get(i).getName()+"  "+Restaurant.allRestaurants.get(i).getPassword()+"  "+Restaurant.allRestaurants.get(i).getManager());
+            System.out.println();
+        }
+        System.out.println("==============================");
+    }
 }
